56ff2dafe4c79569d736baaab33d23afda04e720
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-03-16 12:28:07 +0100

    Page des Booking maj

---
+++ WorkTogether.WPF/AdminView/List/Booking.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BookingData = WorkTogether.Data.Models.Booking;
+
+namespace WorkTogether.WPF.AdminView.List
+{
+    /// <summary>
+    /// Logique d'interaction pour Booking.xaml
+    /// </summary>
+    public partial class Booking : UserControl, IList<BookingData>
+    {
+        private PageList _page;
+        PageList IList<BookingData>.page => _page;
+
+        private BookingData _data;
+        public BookingData Selected_Data => _data;
+
+        public Booking(PageList page)
+        {
+            _page = page;
+            _data = new BookingData();
+            InitializeComponent();
+            load();
+        }
+
+        public void Data_Selected(object sender, RoutedEventArgs e)
+        {
+            _data = DataGrid.SelectedItem as BookingData;
+
+            if (_data != null)
+                _page.setSelectedData<BookingData>(_data);
+        }
+
+        public void load()
+        {
+            DataGrid.ItemsSource = _page.window.context.BookingSet
+                .Include(b => b.Company)
+                .Include(b => b.Individual)
+                .Include(b => b.Offer)
+                .ToList();
+        }
+    }
+}
