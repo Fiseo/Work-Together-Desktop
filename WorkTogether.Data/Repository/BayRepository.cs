4fc8c4c2c8e9e164125f13643afa03c2f6e7c731
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-04-16 13:16:07 +0200

    Override de la fonction de delete dans BayRepository afin de supprimer les unités enfants en meme temps

--- WorkTogether.Data/Repository/BayRepository.cs
+++ WorkTogether.Data/Repository/BayRepository.cs
@@ -10,12 +10,29 @@ namespace WorkTogether.Data.Repository
 {
     public class BayRepository: EntityRepository<Bay>
     {
-        public BayRepository() : base(new WorkTogetherContext()) { }
-        public BayRepository(WorkTogetherContext context) : base(context) { }
+        private UnitRepository _unitRepository;
+        public BayRepository() : this(new WorkTogetherContext()) { }
+        public BayRepository(WorkTogetherContext context) : base(context)
+        {
+            _unitRepository = new UnitRepository(context);
+        }
 
         protected override void setDbSet()
         {
             DbSet = Context.BaySet;
+        }
+
+        public override void delete(Bay entity)
+        {
+            if (entity.isDeletable())
+            {
+                foreach (Unit u in entity.Units)
+                    _unitRepository.delete(u);
+                DbSet.Remove(entity);
+                Context.SaveChanges();
+                return;
+            }
+            throw new Exception("This entity isn't deletable at the moment !");
         }
 
         public override List<Bay> findAll()
