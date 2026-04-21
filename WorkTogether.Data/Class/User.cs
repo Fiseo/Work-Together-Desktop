4d1318dbfef2e08c2b5868eedfa9b404af3fd2e7
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-04-21 11:12:10 +0200

    Rajout des info de validation dans la classe User

--- WorkTogether.Data/Class/User.cs
+++ WorkTogether.Data/Class/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WorkTogether.Data.Models;
 
@@ -8,12 +9,16 @@ public class User : DbEntity
 {
     public int Id { get; set; }
 
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = null!;
 
-    public string Roles { get; set; } = null!;
+    public string Roles { get; set; } = "{ }";
 
+    [Required]
     public string Password { get; set; } = null!;
 
+    [Required]
     public string Username { get; set; } = null!;
 
     public string Type { get; set; } = null!;
